59b777571038e92c8bb47d988ec96bb3da7829cb
Andre Carter <andre.carter@knights.ucf.edu>
2023-02-15 21:47:44 -0500

    added .gitmodule and commented out code in index template

--- src/templates/pages/index-2.js
+++ src/templates/pages/index-2.js
@@ -1,36 +1,36 @@
-import React from "react";
-import { graphql } from "gatsby";
+// import React from "react";
+// import { graphql } from "gatsby";
 
-import Layout from "../../components/layout";
-import Home from "../../pages/home";
+// import Layout from "../../components/layout";
+// import Home from "../../pages/home";
 
-const IndexPage = () => {
-  return (
-    <Layout>
-      <Home 
-        // content={data.home.edges[0].node} 
-      />
-    </Layout>
-  )
-}
+// const IndexPage = () => {
+//   return (
+//     <Layout>
+//       <Home 
+//         // content={data.home.edges[0].node} 
+//       />
+//     </Layout>
+//   )
+// }
 
-export default IndexPage;
+// export default IndexPage;
 
-export const pageQuery = graphql`
-  {
-    home: allMarkdownRemark {
-      edges {
-        node {
-          frontmatter {
-            title
-            greetings
-            emoji
-            subtitlePrefix
-            subtitleHighlight
-          }
-          rawMarkdownBody
-        }
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   {
+//     home: allMarkdownRemark {
+//       edges {
+//         node {
+//           frontmatter {
+//             title
+//             greetings
+//             emoji
+//             subtitlePrefix
+//             subtitleHighlight
+//           }
+//           rawMarkdownBody
+//         }
+//       }
+//     }
+//   }
+// `
